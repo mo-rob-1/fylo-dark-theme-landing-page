a7d0379e251a2ca3b19604f03ed921ffbc36e2f6
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-04-27 14:38:28 +0100

    update

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"F:\\fylo-dark-theme-landing-page\\src\\images\\bg-curvy-mobile.svg":[["bg-curvy-mobile.3eef75f6.svg","images/bg-curvy-mobile.svg"],"images/bg-curvy-mobile.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"F:\\fylo-dark-theme-landing-page\\src\\images\\bg-curvy-mobile.svg":[["bg-curvy-mobile.3eef75f6.svg","images/bg-curvy-mobile.svg"],"images/bg-curvy-mobile.svg"],"F:\\fylo-dark-theme-landing-page\\src\\images\\bg-curvy-desktop.svg":[["bg-curvy-desktop.b3534639.svg","images/bg-curvy-desktop.svg"],"images/bg-curvy-desktop.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61445" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65303" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
